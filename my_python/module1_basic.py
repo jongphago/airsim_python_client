525d456f2deae1bbbe0d43986cdb7fe8a7e8d4ce
jongphago <jongphago@github.com>
2021-11-02 11:23:09 +0900

    Add takeoff and quit function hello_drone.py에 있는 이륙 및 초기화 메서드를 함수로 작성

--- my_python/module1_basic.py
+++ my_python/module1_basic.py
@@ -5,24 +5,24 @@ client = airsim.MultirotorClient()
 client.confirmConnection()
 client.enableApiControl(True)
 
-# get state: 
-# get imu_data
-# get barometer_data
-# get magnetometer_data
-# gps_data
-
 # takeoff
-airsim.wait_key('Press any key to takeoff')
-print("Taking off...")
-client.armDisarm(True)
-client.takeoffAsync().join()
+def takeoff(client):
+    airsim.wait_key('Press any key to takeoff')
+    print("Taking off...")
+    client.armDisarm(True)
+    client.takeoffAsync().join()
+
+takeoff(client)
 
 
 
 
 
 # quit
-airsim.wait_key('Press any key to reset to original state')
-client.reset()
-client.armDisarm(False)
-client.enableApiControl(False)
+def quit(client):
+    airsim.wait_key('Press any key to reset to original state')
+    client.reset()
+    client.armDisarm(False)
+    client.enableApiControl(False)
+
+quit(client)
